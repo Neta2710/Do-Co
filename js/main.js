d3554f9c34f917d30d8027f1d0e57b832407a1cb
Neta <ChetritNetanael@gmail.com>
2022-12-26 19:29:43 +0100

    second commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// var menu = document.getElementById("navBurger")
+var test = 1
+// function openMenu() {
+//     document.querySelector(".test").style.display = "block"
+//     console.log("yes")
+//     openClose++;
+   
+//     check()
+// }
+
+// function check() {
+//  if (openClose == 2) {
+//     console.log("yes2")
+//     menu.style.display = "flex !important"
+//     openClose--
+//  }
+//  menu.style.display = "none"
+// }
+
+
+// menu.style.display = "none"
+
+function openMenu() {
+    if (test == 1) {
+        document.getElementById("navBurger").style.display = "flex";
+        document.getElementById("navBurger").style.height = "215px"
+        document.getElementById("titleMenu").style.top = "5px"
+        test++
+    } else {
+        document.getElementById("navBurger").style.display = "none";
+        test--
+    }
+
+}
+
